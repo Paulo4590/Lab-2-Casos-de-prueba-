9002e61481f619a14d4f28d6f60150b2072581ff
Paulo4590 <145291144+Paulo4590@users.noreply.github.com>
2026-09-11 01:40:11 +0000

    lab-3

--- test_lab2.py
+++ test_lab2.py
@@ -1,62 +1,107 @@
 import ecommerce_form
 import logging
+import pytest
 
-logging .basicConfig(
-    level= logging.DEBUG,
+logging.basicConfig(
+    level=logging.DEBUG,
     filemode='w',
     filename='test.log'
 )
 
+@pytest.fixture
+def system():
+    return ecommerce_form.OnlinePurchase()
+
+@pytest.mark.parametrize("quantity, expected", [(5, True), (0, False), (-1, False), (.8, False)])
+
+@pytest.mark.unit 
+def test_validate_quantity_compontent(system, quantity, expected):
+    result = system.validate_quantity(quantity)
+    assert result == expected
+
+
+
+@pytest.mark.parametrize("coupon, expected", [('DISCOUNT10', True), ('DISCOUNT30', False),('DISCOUNT20', True),('DISCOUNT40', False)])
+
+@pytest.mark.unit
+def test_validate_coupon(system, coupon, expected):
+    result = system.validate_coupon(coupon)
+    assert result == expected   
+
+
+
+@pytest.mark.parametrize("address, expected", [('Av patria', True), ('tres', False),  ('123456', False)])
+
+@pytest.mark.wip
+def test_validate_address(system, address, expected):
+    result = system.validate_address(address)
+    assert result == expected   
+
+
+
+
+@pytest.mark.system
+
 def test_purchase_itemzero():
     logging.info('TEST CASE 1')
     system = ecommerce_form.OnlinePurchase()
-
-    cart ={
-        'Laptop': 0,
-        'Mouse': 2
+    
+    cart = {
+        'laptop': 0,
+        'mouse': 2
     }
-
     coupon = 'DISCOUNT10'
-    address = 'Av Patria'
-    result = system.process_purchase(cart,coupon,address)
+    address = 'av patria'
+    result = system.process_purchase(cart, coupon, address)
+    
+    assert 'integer greater than 0' in result
+    
+    logging.info('TEST CASE finished')
 
-    assert 'integer greater than 0' in result, f"Expected error message for invalid quantity, but got: {result}"
+@pytest.mark.system
 
-    logging.info(f'TEST CASE FINISHED')
+def test_invalid_coupon():
+    logging.info('TEST CASE 2')
+    system = ecommerce_form.OnlinePurchase()
+    
+    cart = {
+        'laptop': 1,
+        'mouse': 2
+    }
+    coupon = 'DISCOUNT30'
+    address = 'av patria'
+    result = system.process_purchase(cart, coupon, address)
+    
+    assert 'coupon code is not valid' in result
+    logging.info(f'Invalid coupon test result: {result}')
+    
+    logging.info('TEST CASE finished')
 
-def test_purchase_invalid_coupon():
+
+@pytest.mark.system
+    
+def test_valid_coupon():
     logging.info('TEST CASE 3')
     system = ecommerce_form.OnlinePurchase()
-
-    cart ={
-        'Laptop': 1,
-        'Mouse': 2
+    
+    cart = {
+        'laptop': 1,
+        'mouse': 2
     }
-
-    coupon = 'INVALID'
-    address = 'Av Patria'
-    result = system.process_purchase(cart,coupon,address)
-
-    assert 'code is not valid' in result, f"Expected error message for invalid coupon, but got: {result}"
-
-    logging.info(f'The purchase result is: {result}')
-    logging.info(f'TEST CASE 3 FINISHED')
-
-def test_purchase_valid_coupon():
-    logging.info('TEST CASE 9')
-    system = ecommerce_form.OnlinePurchase()
-
-    cart ={
-        'Laptop': 1,
-        'Mouse': 2
-    }
-
     coupon = 'DISCOUNT10'
-    address = 'Av Patria'
-    result = system.process_purchase(cart,coupon,address)
+    address = 'av patria'
+    result = system.process_purchase(cart, coupon, address)
+    
+    assert 'Error' not in result
+    assert '990'  in result
 
-    assert 'DISCOUNT10' in result
-    assert '935' in result
+    logging.info(f'Valid coupon test result: {result}')
+    
+    logging.info('TEST CASE finished')
 
-    logging.info(f'The purchase result is: {result}')
-    logging.info(f'TEST CASE 9 FINISHED')
+
+
+
+if __name__ == '__main__':
+    test_purchase_itemzero()
+    test_valid_coupon()
